d0e777054ee353e21642890804ce569d1fab8f8a
Amit Krishna <23555312+amitkr936@users.noreply.github.com>
2020-08-15 20:23:09 +0530

    feat:FAQ Page complete

--- Jquery/onemonthjquery/project-2/README.md
+++ Jquery/onemonthjquery/project-2/README.md
@@ -40,8 +40,51 @@ index.js
 $("#q1").click(function() {
  $("#a1").slideToggle("slow");
 })'''
+## Animations
+[Jquery](https://api.jquery.com/category/effects/custom-effects/) allows to do custom animations in UI.
+1. Identify toggle selectors
+index.html
+'''
+<img id="arrow1-down" class="arrow down" src ="assets/arrow-down.svg">
+<img id="arrow1-up" class="arrow up" src ="assets/arrow-up.svg">
+'''
 
+2. Add collapse functionality to up and down arrows
+index.js
+'''
+$("#q1").click(function() {
+ $("#a1").fadeToggle("fast");
+ $("#arrow1-down").toggleClass("collapse");
+ $("#arrow1-up").toggleClass("collapse");
+})
+'''
+3. Combine functionality for the same class
+index.js
+'''
+$("#q1").click(function() {
+ $("#a1").fadeToggle("fast");
+ $("#arrow1-down, #arrow1-up").toggleClass("collapse");
+})'''
 
+4. Add functionality to each FAQ question, answer and arrows
+index.js
+'''
+$("#q1").click(function() {
+ $("#a1").fadeToggle("fast");
+ $("#arrow1-down, #arrow1-up").toggleClass("collapse");
+})
+
+$("#q2").click(function() {
+ $("#a2").fadeToggle("fast");
+ $("#arrow2-down, #arrow2-up").toggleClass("collapse");
+})
+
+$("#q3").click(function() {
+ $("#a3").fadeToggle("fast");
+ $("#arrow3-down, #arrow3-up").toggleClass("collapse");
+})
+'''
+## DOM in Depth:-
 
 
 
--- Jquery/onemonthjquery/project-2/index.js
+++ Jquery/onemonthjquery/project-2/index.js
@@ -1,14 +1,39 @@
-$( "#q1" ).click(function($event) {
-    console.log($("#q1"));
-    $( "#a1").slideToggle("slow", function (){
-        console.log("Animation completed");
-    })
+$( ".question" ).click(function($event) {
+    // console.log($("#q1"));// DOM Node
+    console.log($(this).next());
+    console.log($(this).children());
+    $(this).next().fadeToggle("fast");
+    $(this).children().toggleClass("collapse");
+    console.log($(this).next().next().animate({
+        opacity: 1.25,
+        height: "200px"
+      },2000, function() {
+        // Animation complete.
+      }
+    ));
+    // $( "#a1").fadeToggle("slow");
+    // $("#arrow1-down,  #arrow1-up").addClass("collapse");
   });
+/*$( "#q2" ).click(function($event) {
+console.log($("#q1"));// DOM Node
+    $( "#a2").fadeToggle("slow", function (){
+        console.log($("#a1"));
+});
+$("#arrow2-down,  #arrow2-up").addClass("collapse");
+});
 
+$( "#q3" ).click(function($event) {
+    console.log($("#q1"));// DOM Node
+    $( "#a3").fadeToggle("slow", function (){
+        console.log($("#a1"));
+    });
+    $("#arrow3-down,  #arrow3-up").addClass("collapse");
+  });*/
+/*
  $("#q2").click(function(){
    $("#a2").slideToggle("slow",function (){
      console.log("Animate completed");
  })
- }) 
-
+ }); 
+*/
   // $() is short-end for jquery()

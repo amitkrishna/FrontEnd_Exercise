2ec1bfcbba4ce22449f651d16fb8e688ca49b359
Amit Krishna <23555312+amitkr936@users.noreply.github.com>
2020-08-16 18:08:52 +0530

    bugfix

--- Jquery/onemonthjquery/project-3/index.js
+++ Jquery/onemonthjquery/project-3/index.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
     // YOUR CODE HERE!
     $(".add-items").submit(function ( $event ){
-        console.log("Hit");
         $event.preventDefault();
-        console.log($event);
         var item = $("#todo-list-item").val();
-        console.log(item);
-        if(item){// run only if condition is true
-            $("#todo-list-item").val("");
-
+        console.log($("#todo-list-item").val());
+        console.log(typeof(item));
+        console.log(typeof(item.val));// undefined
+        if (Boolean(item.val)) {// run only if condition is true
+            console.log(Boolean(item.val));// gets the value of the item
+            console.log(typeof(item));
+            console.log("Hit");
         $("#list-items").append("<li><input type='checkbox'/>" + item + "<a class='remove'>x</a></li>");
+        $("#todo-list-item").val("");
         }
     });
-
 });
